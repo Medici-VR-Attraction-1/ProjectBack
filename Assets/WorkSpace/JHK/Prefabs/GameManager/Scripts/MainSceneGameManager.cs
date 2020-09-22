eaccd0c27bb0b922bcb459d59e65eb2dcc55241b
ju6579 <ju6579@naver.com>
2020-09-22 09:14:48 +0900

    Project Basic Initialize
    [Main Scene Game Manager]
    -Singleton Object
    -Manage Main Scene
    
    [Player Prefab]
    -Basic Player Prefab For K&M

---
+++ Assets/WorkSpace/JHK/Prefabs/GameManager/Scripts/MainSceneGameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.OpenVR;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.XR;
+using Valve.VR;
+
+public class MainSceneGameManager : MonoBehaviour
+{
+    private static MainSceneGameManager _instance = null;
+
+    #region Public Static Method
+    public static MainSceneGameManager GetInstance() { return _instance; }
+    #endregion
+
+    #region Public Method
+    public void AddPlayerScore(int value) { _playerScore += value; }
+    #endregion
+
+    [SerializeField]
+    private GameObject VRPlayerInstance = null;
+
+    [SerializeField]
+    private GameObject KMPlayerInstance = null;
+
+    [SerializeField]
+    private Transform PlayerStartPoint = null;
+
+    private GameObject _playerInstance = null;
+    private int _playerScore = 0;
+
+    #region MonoBehaviour Callbacks
+    private void Start()
+    {
+        // Check and Set Singleton Object
+        if(_instance != null)
+        {
+            Debug.Log("MainSceneGameManager : Game Manager has Duplicated, Delete Another One");
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        // Check Property Set Collectly and Instatiate by Player
+        if (!CheckIsPropertySet())
+        {
+            Debug.Log("MainSceneGameManager : Gama Manager Property has Not Set, Check Properties");
+            gameObject.SetActive(false);
+            Application.Quit();
+        }
+        else
+        {
+            GameObject targetPrefab = XRDevice.isPresent ? VRPlayerInstance : KMPlayerInstance;
+            _playerInstance = Instantiate(targetPrefab, PlayerStartPoint.position, PlayerStartPoint.rotation);
+        }
+    }
+    #endregion
+
+    #region Private Method
+    // Check Serialize Field is Set On Inspector. If Properties all Set, return true.
+    private bool CheckIsPropertySet()
+    {
+        bool checkResult = VRPlayerInstance != null
+                         && KMPlayerInstance != null
+                         && PlayerStartPoint != null;
+        return checkResult;
+    }
+    #endregion
+}
