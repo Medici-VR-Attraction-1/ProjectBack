38c4050193d70af0f58434c84472d43c368f5ead
ju6579 <ju6579@naver.com>
2020-09-22 17:05:12 +0900

    [Code Review] Guest Controller
    - Move AI Coroutine 변경
    
    #issue: GuestController->Sit()
    Target Forward 방향으로 회전할 것

--- Assets/WorkSpace/CYJ/Guest/GuestController.cs
+++ Assets/WorkSpace/CYJ/Guest/GuestController.cs
@@ -6,36 +6,46 @@ using UnityEngine.AI;
 public class GuestController : MonoBehaviour
 {
     [SerializeField]
-    GameObject Target;
+    private GameObject Target;
 
-    NavMeshAgent _navMeshAgent;
-    Animator _animator;
-    void Start()
+    [SerializeField,Range(1.5f, 2.5f)]
+    private float targetDistanceOffset = 1.5f;
+
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    private WaitForSeconds _pathfindRate = new WaitForSeconds(0.1f);
+
+    private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
 
-        
-        _navMeshAgent.SetDestination(Target.transform.position);
         _navMeshAgent.speed = 3.5f;
+        StartCoroutine(_MoveToChair());
     }
 
-    void Update()
+    private void Update()
     {
-        if (Vector3.Distance(this.transform.position,Target.transform.position) < 1)
-        {
-            Target = GameObject.Find("Chair");
-            _navMeshAgent.SetDestination(Target.transform.position);
-            if (Vector3.Distance(this.transform.position, Target.transform.position) < 1)
-            {
-                Sit();
-            }
-        }
+
     }
 
-    public void Sit()
+    private void Sit()
     {
-        _navMeshAgent.speed = 0;
+        _navMeshAgent.isStopped = true;
+        // Guest Transform 방향 의자 방향으로 변경할 것
         _animator.SetTrigger("Sit");
+    }
+
+    private IEnumerator _MoveToChair()
+    {
+        while(Vector3.Distance(this.transform.position, Target.transform.position) > 1)
+        {
+            _navMeshAgent.SetDestination(Target.transform.position
+                                     + Target.transform.forward * targetDistanceOffset);
+            yield return _pathfindRate;
+        }
+
+        Sit();
+        yield return null;
     }
 }
