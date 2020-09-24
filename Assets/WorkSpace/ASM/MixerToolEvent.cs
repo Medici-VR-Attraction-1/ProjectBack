8584b3655b562d0981c9148f7aac09f97bd006bd
ju6579 <ju6579@naver.com>
2020-09-24 20:01:55 +0900

    Merge Fix
    Merge Fix

--- Assets/WorkSpace/ASM/MixerToolEvent.cs
+++ Assets/WorkSpace/ASM/MixerToolEvent.cs
@@ -8,14 +8,19 @@ public class MixerToolEvent : MonoBehaviour
     private float CookingSpeed = 0.1f;
 
     [SerializeField]
-    private GameObject Juice;
+    private GameObject Juice = null;
 
     private Dictionary<int, Transform> _ingredientTransform = new Dictionary<int, Transform>();
+    private MeshRenderer _juiceMeshRenderer = null;
+    private Color _originalColor;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (Juice == null) Debug.Log("MixerToolEvent: No Inspector Value");
 
+        _juiceMeshRenderer = Juice.GetComponentInChildren<MeshRenderer>();
+        _originalColor = _juiceMeshRenderer.material.color;
     }
 
     // Update is called once per frame
@@ -33,17 +38,17 @@ public class MixerToolEvent : MonoBehaviour
 
                 if (targetTranform != null)
                 {
-                //크기가 줄어든다
-                targetTranform.localScale += new Vector3(-1, -1, -1) * Time.deltaTime * 0.8f;
-                //특정 크기 밑이 되면 삭제
-                if (targetTranform.localScale.y < 0.01)
-                {
-                    Destroy(targetTranform.gameObject);
-                    print("Destroy");
-                }
-                //가능하면 물색도 같이 바꿔주기 해당 재료색으로
-                Juice.GetComponentInChildren<MeshRenderer>().material.color = Color.Lerp(Juice.GetComponentInChildren<MeshRenderer>().material.color, targetTranform.GetComponent<MeshRenderer>().material.color, Time.deltaTime * 3);
-                // 액체를 채워줍시다     
+                    //크기가 줄어든다
+                    targetTranform.localScale += new Vector3(-1, -1, -1) * Time.deltaTime * 0.8f;
+                    //특정 크기 밑이 되면 삭제
+                    if (targetTranform.localScale.y < 0.01f)
+                    {
+                        Destroy(targetTranform.gameObject);
+                        print("Destroy");
+                    }
+                    //가능하면 물색도 같이 바꿔주기 해당 재료색으로
+                    _juiceMeshRenderer.material.color = Color.Lerp(_juiceMeshRenderer.material.color, targetTranform.GetComponent<MeshRenderer>().material.color, Time.deltaTime * 3f);
+                    // 액체를 채워줍시다     
                     Vector3 juiceEmty = Juice.transform.localScale;
                     Vector3 juiceFull = juiceEmty + Vector3.up;
                     Juice.transform.localScale = Vector3.Lerp(juiceEmty, juiceFull, Time.deltaTime);
